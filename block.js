644c95f6e52489b968c441e5f5423ffb098e7d8b
Oliver Turnbull <oturnbull1@gmail.com>
2020-04-08 12:46:24 +0100

    Version 0.1 finished

--- block.js
+++ block.js
@@ -1,12 +1,18 @@
+
+
 function deleteFeed() {
 
-var old_feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream], [id^=stories_pagelet_below_composer]');
-var new_feed = $('[data-pagelet=Stories], [role=feed]');
 
-old_feed.remove();
-new_feed.remove();
+    if   (window.location.href == "https://www.facebook.com/" ){
+    console.log('heeeey');
+    var old_feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream], [id^=stories_pagelet_below_composer]');
+    var new_feed = $('[data-pagelet=Stories], [role=feed]');
 
+    old_feed.remove();
+    new_feed.remove();
+    };
 }
 
 // Prevent reloading of the newsfeed
-window.setInterval(deleteFeed, 50);
+
+var interval = setInterval(deleteFeed, 50);
